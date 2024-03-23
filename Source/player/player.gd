c72a3328bc7a8d33e9f3a54f4d18576c378e54a4
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-03-23 18:42:09 +0100

    Import Tilemap and construct the world
    Animated flower and trees with player behind

--- Source/player/player.gd
+++ Source/player/player.gd
@@ -1,12 +1,26 @@
 extends CharacterBody2D
 
-@export var speed: int = 35
+@export var speed: int = 75
+@onready var animations = $AnimationPlayer
 
 func handleInput():
 	var move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = move_direction*speed
 
+func updateAnimation():
+	if velocity.length() == 0:
+		if animations.is_playing(): animations.stop()
+		return
+	var direction = "Down"
+	if velocity.x < 0: direction = "Left"
+	elif velocity.x > 0: direction = "Right"
+	elif velocity.y < 0: direction = "Up"
+
+	animations.play("walk" + direction)
+	
+
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	updateAnimation()
 
