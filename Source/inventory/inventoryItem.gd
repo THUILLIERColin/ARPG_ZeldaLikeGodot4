998b87f61db2d906dc4cfb0de093f8c4dac6d15a
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-03-27 18:47:39 +0100

    Limit amount of item per stack

--- Source/inventory/inventoryItem.gd
+++ Source/inventory/inventoryItem.gd
@@ -4,4 +4,4 @@ class_name InventoryItem
 
 @export var name: String
 @export var texture: Texture2D
-@export var maxAmoutPrStack: int
+@export var max_amount_per_stack: int
--- Source/inventory/items/inventory.gd
+++ Source/inventory/items/inventory.gd
@@ -6,14 +6,38 @@ signal updated
 
 @export var slots: Array[InventorySlot]
 
-func insert(item: InventoryItem):
-	var itemSlots = slots.filter(func(slot): return slot.item == item)
+func insert_in_empty_slot(item: InventoryItem):
+	var emptySlots = slots.filter(func(slot): return slot.item == null) # filtre les slots vides
+	if !emptySlots.is_empty():
+		emptySlots[0].item = item
+		emptySlots[0].amount = 1
+
+func insert2(item: InventoryItem):
+	var itemSlots = slots.filter(func(slot): return slot.item == item) # filtre les slots qui contiennent l'item
 	if !itemSlots.is_empty():
-		itemSlots[0].amount += 1
+		if itemSlots[0].amount < item.max_amount_per_stack:
+			itemSlots[0].amount += 1
+		else:
+			insert_in_empty_slot(item)
 	else:
-		var emptySlots = slots.filter(func(slot): return slot.item == null)
-		if !emptySlots.is_empty():
-			emptySlots[0].item = item
-			emptySlots[0].amount = 1
+		insert_in_empty_slot(item)
 	
 	updated.emit()
+
+func insert(item: InventoryItem):
+	# Cherche d'abord un slot avec l'item qui n'est pas plein.
+	var itemSlots = slots.filter(func(slot): return slot.item == item and slot.amount < item.max_amount_per_stack)
+	
+	if itemSlots:
+		# Si trouvé, augmente la quantité de 1.
+		itemSlots[0].amount += 1
+	else:
+		# Sinon, cherche un slot vide.
+		var emptySlots = slots.filter(func(slot): return slot.item == null)
+		if emptySlots:
+				# Si un slot vide est trouvé, initialise l'item et la quantité à 1.
+			emptySlots[0].item = item
+			emptySlots[0].amount = 1
+
+	updated.emit()
+
