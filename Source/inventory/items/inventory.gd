6233fa4b21c200dd053d923466b62afbb31146b8
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-03-27 18:48:39 +0100

    Delete insert2 deprecated function

--- Source/inventory/items/inventory.gd
+++ Source/inventory/items/inventory.gd
@@ -6,24 +6,6 @@ signal updated
 
 @export var slots: Array[InventorySlot]
 
-func insert_in_empty_slot(item: InventoryItem):
-	var emptySlots = slots.filter(func(slot): return slot.item == null) # filtre les slots vides
-	if !emptySlots.is_empty():
-		emptySlots[0].item = item
-		emptySlots[0].amount = 1
-
-func insert2(item: InventoryItem):
-	var itemSlots = slots.filter(func(slot): return slot.item == item) # filtre les slots qui contiennent l'item
-	if !itemSlots.is_empty():
-		if itemSlots[0].amount < item.max_amount_per_stack:
-			itemSlots[0].amount += 1
-		else:
-			insert_in_empty_slot(item)
-	else:
-		insert_in_empty_slot(item)
-	
-	updated.emit()
-
 func insert(item: InventoryItem):
 	# Cherche d'abord un slot avec l'item qui n'est pas plein.
 	var itemSlots = slots.filter(func(slot): return slot.item == item and slot.amount < item.max_amount_per_stack)
